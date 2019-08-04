0bfa95969018506fb95c29290c766fb5d33a990f
Bruno <brunoalvescaixeta@hotmail.com>
2019-08-04 15:56:23 -0300

    Initial commit

---
+++ Biosis/AnalysisDataExtract.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Biosis
+{
+    public class AnalysisDataExtract
+    {
+        public void AnalysisFileProcess(string file)
+        {
+            var data = Convert.FromBase64String(file);
+            File.WriteAllBytes("../AnalysisFile.txt", data);
+            var lines = File.ReadAllLines("../AnalysisFile.txt");
+        }
+    }
+}
---
+++ Biosis/Controllers/AnalysisController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Biosis.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AnalysisController : ControllerBase
+    {
+        // GET api/values
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public ActionResult<string> Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody] AnalysisFileDTO analysisFileDTO)
+        {
+            AnalysisDataExtract dataExtract = new AnalysisDataExtract();
+            dataExtract.AnalysisFileProcess(analysisFileDTO.Base64);
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
