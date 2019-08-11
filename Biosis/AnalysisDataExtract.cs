5254004325223447d72eea43109f514d34b58e00
Bruno <brunoalvescaixeta@hotmail.com>
2019-08-10 22:32:34 -0300

    Set rules for type classification

--- Biosis/AnalysisDataExtract.cs
+++ Biosis/AnalysisDataExtract.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace Biosis
@@ -13,6 +14,53 @@ namespace Biosis
             var data = Convert.FromBase64String(file);
             File.WriteAllBytes("../AnalysisFile.txt", data);
             var lines = File.ReadAllLines("../AnalysisFile.txt");
+
+            var msp = 0;
+            var msg = 0;
+            var mg = 0;
+            var total = 0;
+
+            foreach(var wing in lines)
+            {
+                if (wing[0] == char.Parse("*") || !Regex.IsMatch(wing, @"[0-9]"))
+                {
+                    continue;
+                }
+                else
+                {
+                    //var sectors = wing.Split(" ");
+                    var sectors = Regex.Matches(wing, @"[A-Z]'?(\d+)?-\d+");
+                    foreach(Match sector in sectors)
+                    {                        
+                        var split = sector.ToString().Split("-");
+                        var identifier = Regex.Match(split[0], @"\d+").Value;
+
+                        if(string.IsNullOrWhiteSpace(identifier))
+                        {
+                            identifier = "1";
+                        }
+
+                        var postTrace = split[1];
+                        if((int.Parse(identifier) <= 2 && int.Parse(postTrace) == 0) || (int.Parse(identifier) == 0 && (int.Parse(postTrace) <= 2)))
+                        {
+                            msp += 1;
+                        }
+                        else if((int.Parse(identifier) == 0 && int.Parse(postTrace) > 2) || (int.Parse(identifier) > 2 && int.Parse(postTrace) == 0))
+                        {
+                            msg += 1;
+                        }
+                        else //if(int.Parse(identifier) > 0 && int.Parse(postTrace) > 0)
+                        {
+                            mg += 1;
+                        }                        
+                    }
+                        
+                }                
+
+            }
+            total = msp + msg + mg;
         }
+
+
     }
 }
